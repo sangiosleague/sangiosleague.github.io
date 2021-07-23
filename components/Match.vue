d0362bb4408fce8c9d00eb4c6da249b1d33f548b
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-07-23 12:58:17 +0200

    component with parameters !

---
+++ components/Match.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-card
+    :title="title"
+    tag="article"
+  >
+    <b-row no-gutters>
+      <b-card-text>
+        Some quick example text to build on the card title and make up the bulk of the card's content.
+      </b-card-text>
+
+      <b-button href="#" variant="primary">
+        Go somewhere
+      </b-button>
+    </b-row>
+  </b-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'default title',
+      required: true
+    }
+  }
+}
+</script>
--- pages/matches.vue
+++ pages/matches.vue
@@ -14,21 +14,7 @@
             v-for="card in slide"
             :key="card"
           >
-            <b-card
-
-              :title="card"
-              tag="article"
-            >
-              <b-row no-gutters>
-                <b-card-text>
-                  Some quick example text to build on the card title and make up the bulk of the card's content.
-                </b-card-text>
-
-                <b-button href="#" variant="primary">
-                  Go somewhere
-                </b-button>
-              </b-row>
-            </b-card>
+            <Match :title="card" />
           </div>
         </swiper-slide>
       </swiper>
