c1bf99e37d4dfc3ccac14e702d1c4e3fd95d72ed
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-07-23 15:36:09 +0200

    better embryonic redis

--- api/count.js
+++ api/count.js
@@ -1,15 +1,20 @@
-import Redis from 'ioredis'
+/* import Redis from 'ioredis' */
 
+export default function (req, res) {
+/*
 export default async function (req, res) {
   const redis = new Redis({
     port: 11579, // Redis port
     host: 'redis-11579.c72.eu-west-1-2.ec2.cloud.redislabs.com', // Redis host
     family: 4, // 4 (IPv4) or 6 (IPv6)
-    /* password: '___', */
+    // password: '',
     db: 0
   })
   const count = await redis.incr('counter')
   redis.quit()
+*/
+
+  const count = 12
 
   res.setHeader('Content-Type', 'application/json')
   res.write(JSON.stringify({ count }))
