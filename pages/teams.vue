15889ce810ad338679ab90d602897805dee0c6b7
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-07-25 20:57:48 +0200

    avoid route conflicts

--- pages/teams.vue
+++ pages/teams.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <h1>teams</h1>
-    <b-img v-for="team in teams" :key="team" :src="`/teams/${team}.png`" fluid alt="Fluid image" />
+    <b-img v-for="team in teams" :key="team" :src="`/images/teams/${team}.png`" fluid alt="Fluid image" />
   </div>
 </template>
 
 <script>
 export default {
   async asyncData ({ $axios }) {
-    const teams = await $axios.$get('/teams/teams.json')
+    const teams = await $axios.$get('/api/teams.json')
     return { teams }
   }
 }
