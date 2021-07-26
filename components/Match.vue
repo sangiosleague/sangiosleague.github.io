7ddb6c71520ca37f3ad1cc08594001abf3372434
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-07-27 01:05:41 +0200

    start fixtures

--- components/Match.vue
+++ components/Match.vue
@@ -1,27 +1,38 @@
 <template>
-  <b-card
-    :title="title"
-    tag="article"
-  >
-    <b-row no-gutters>
-      <b-card-text>
-        Some quick example text to build on the card title and make up the bulk of the card's content.
-      </b-card-text>
-
-      <b-button href="#" variant="primary">
-        Go somewhere
-      </b-button>
-    </b-row>
+  <b-card>
+    <!-- :title="`${fixture.teams[0].id} ${fixture.teams[0].goals} - ${fixture.teams[1].goals} ${fixture.teams[1].id}`" tag="article"
+    class="d-flex justify-content-center align-items-center bd-highlight mb-3"-->
+    <div style="font-size: smaller; font-style: italic;">
+      {{ $moment(fixture.when).format('LLLL') }}
+    </div>
+    <div class="d-flex justify-content-center align-items-center bd-highlight mb-3">
+      <div class="flex-fill">{{ fixture.teams[0].id }}</div>
+      <div class="flex-fill" style="margin: 0 1rem; font-family: monospace;">
+        <span style="font-size: larger;">{{ fixture.teams[0].goals }}</span>
+        <font-awesome-icon :icon="['fas', 'minus']" />
+        <span style="font-size: larger;">{{ fixture.teams[1].goals }}</span>
+      </div>
+      <div class="flex-fill">{{ fixture.teams[1].id }}</div>
+    </div>
   </b-card>
 </template>
 
 <script>
 export default {
   props: {
-    title: {
-      type: String,
-      default: 'default title',
-      required: true
+    fixture: {
+      type: Object,
+      default: () => {
+        return {
+          when: '2020-08-08T22:00:00+02:00',
+          group: 'A',
+          teams: [
+            { id: 'TDEU', goals: 7 },
+            { id: 'Wagliun20', goals: 1 }
+          ]
+        }
+      },
+      required: false
     }
   }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -113,7 +113,7 @@ export default {
 
   fontawesome: {
     icons: {
-      solid: ['faHome', 'faList', 'faFlag', 'faFutbol', 'faEllipsisH', 'faArrowLeft']
+      solid: ['faHome', 'faList', 'faFlag', 'faFutbol', 'faEllipsisH', 'faArrowLeft', 'faMinus', 'faDotCircle']
     }
   },
 
