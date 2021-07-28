e0f45cf451630c10378b4dec3d62d3fb6465b71a
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-07-28 12:34:30 +0200

    cleaning

--- pages/login.vue
+++ pages/login.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <b-alert v-if="errorMessage" show variant="danger">
+    <!-- b-alert v-if="errorMessage" show variant="danger">
       {{ errorMessage }}
-    </b-alert>
+    </!-->
     <b-alert v-if="$auth.$state.redirect" show>
       You have to login before accessing to
       <strong>{{ $auth.$state.redirect }}</strong>
     </b-alert>
     <b-row align-h="center" class="pt-4">
       <b-card v-show="!loggedIn" title="Login" bg-variant="light">
-        <div class="alert alert-warning">
+        <!-- div class="alert alert-warning">
           <font-awesome-icon :icon="['fas', 'exclamation-triangle']" />
           {{ "loginWarning" }}
-        </div>
+        </!-->
         <div v-for="s in strategies" :key="s.key" class="mb-2" style="width:100%">
           <b-btn
             block
@@ -21,8 +21,7 @@
             :disabled="!s.active"
             @click="$auth.loginWith(s.key)"
           >
-            <!-- font-awesome-icon :icon="['fab', s.key]" /-->
-            {{ "loginPrompt" }} {{ s.name }}
+            {{ s.name }}
           </b-btn>
         </div>
       </b-card>
@@ -50,24 +49,7 @@ export default {
     ...mapState('auth', ['loggedIn']),
     strategies: () => [
       { key: 'facebook', name: 'Facebook', color: '#3c65c4', active: true }
-    ],
-    errorMessage () {
-      const { error } = this
-      if (!error || typeof error === 'string') {
-        return error
-      }
-      let msg = ''
-      if (error.message) {
-        msg += error.message
-      }
-      if (error.errors) {
-        msg += `(${JSON.stringify(error.errors)
-          .replace(/[{}"[\]]/g, '')
-          .replace(/:/g, ': ')
-          .replace(/,/g, ' ')})`
-      }
-      return msg
-    }
+    ]
   },
   methods: {}
 }
