e6ca638951a7e4e37ee4a81dce00fa8303d6d2c8
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-07-28 12:42:07 +0200

    bof

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,9 +3,9 @@
   <div class="d-flex flex-column vh-100">
     <header class="header my-right-fixed-top text-right text-light p-2">
       <client-only>
-        <NuxtLink to="/login">
-          <b-button variant="secondary" class="rounded-circle" style="padding: .3rem">
-            <template v-if="$auth.$state.loggedIn">
+        <b-button class="rounded-circle">
+          <template v-if="$auth.$state.loggedIn">
+            <NuxtLink to="/logout">
               <b-img
                 :src="$auth.user.picture.data.url"
                 class="mt-1"
@@ -13,12 +13,14 @@
                 width="30px"
                 height="30px"
               />
-            </template>
-            <template v-else>
+            </NuxtLink>
+          </template>
+          <template v-else>
+            <NuxtLink to="/login">
               <font-awesome-icon :icon="['fas', 'user']" style="width: 18px; height: 18px;" />
-            </template>
-          </b-button>
-        </NuxtLink>
+            </NuxtLink>
+          </template>
+        </b-button>
       </client-only>
     </header>
     <main role="main">
---
+++ pages/logout.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <!-- b-alert v-if="errorMessage" show variant="danger">
+      {{ errorMessage }}
+    </!-->
+    <b-alert v-if="$auth.$state.redirect" show>
+      You have to login before accessing to
+      <strong>{{ $auth.$state.redirect }}</strong>
+    </b-alert>
+    <b-row align-h="center" class="pt-4">
+      <b-card v-show="!loggedIn" title="Login" bg-variant="light">
+        <!-- div class="alert alert-warning">
+          <font-awesome-icon :icon="['fas', 'exclamation-triangle']" />
+          {{ "loginWarning" }}
+        </!-->
+        <div v-for="s in strategies" :key="s.key" class="mb-2" style="width:100%">
+          <b-btn
+            block
+            :style="{ background: s.color }"
+            class="login-button"
+            :disabled="!s.active"
+            @click="$auth.loginWith(s.key)"
+          >
+            {{ s.name }}
+          </b-btn>
+        </div>
+      </b-card>
+      <b-card v-show="loggedIn" title="Logout" bg-variant="light">
+        <b-btn
+          v-show="loggedIn"
+          block
+          class="login-button"
+          @click="$auth.logout()"
+        >
+          <font-awesome-icon :icon="['fas', 'sign-out-alt']" />
+        </b-btn>
+      </b-card>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  components: {},
+  middleware: ['auth'],
+  computed: {
+    ...mapState('auth', ['loggedIn']),
+    strategies: () => [
+      { key: 'facebook', name: 'Facebook', color: '#3c65c4', active: true }
+    ]
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+.login-button {
+  border: 0;
+}
+</style>
