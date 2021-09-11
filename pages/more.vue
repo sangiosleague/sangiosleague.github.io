0406a3baa33f6a7619ec8857adf4bdad55c3939d
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-09-11 15:47:29 +0200

    2020 !

--- pages/more.vue
+++ pages/more.vue
@@ -4,7 +4,7 @@
       <font-awesome-icon :icon="['fas', 'ellipsis-h']" />
     </h1>
     <div class="container">
-      <b-card v-if="$store.state.year === '2021'" id="winners">
+      <b-card v-if="$store.state.year === '2020'" id="winners">
         <a href="/resources/2020/Regolamento%20Sangios%20League%202020.pdf" target="_blank">
           Sangios League 2020 Regolamento Ufficiale <font-awesome-icon :icon="['fas', 'external-link-alt']" />
         </a>
